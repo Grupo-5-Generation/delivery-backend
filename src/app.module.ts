303015685a8e97589d38530061aee7c5a3640cf6
Gih <giovannabreinackcolombara@gmail.com>
2025-07-10 11:01:21 -0300

    criação do crud de categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234567',
+      database: 'db_delivery',
+      entities: [Categoria],
+      synchronize: true,
+      logging: true,
+    }),
+    CategoriaModule
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// id, tipo e descrição
+
+@Entity({ name: "tb_categorias" })
+export class Categoria {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    tipo: string
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    descricao: string
+
+    // @OneToMany(() => Produto, (produto) => produto.categoria)
+    // produto: Produto[]
+
+}
