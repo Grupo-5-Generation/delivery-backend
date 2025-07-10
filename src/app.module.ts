28ebd0401e8445ffc17da6746460568e3dfa107f
maridevdata <marianaluisa.works@gmail.com>
2025-07-10 12:42:02 -0300

    Criação Classe Usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_delivery',
+      entities: [Usuario],
+      synchronize: true,
+    }),
+    UsuarioModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
