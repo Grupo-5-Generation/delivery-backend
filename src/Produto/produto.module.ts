daedd0c24b106b8e09e2a60cf7829b78d10597c9
giulia <giuliabarreto2@gmail.com>
2025-07-10 11:01:55 -0300

    crud produto finalizado

---
+++ src/Produto/produto.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './entities/produto.entity';
+import { ProdutoService } from './services/produto.service';
+import { ProdutoController } from './controller/produto.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Produto]),
+  ],
+  controllers: [ProdutoController],
+  providers: [ProdutoService],
+  exports: [ProdutoService],
+})
+export class ProdutoModule {}
